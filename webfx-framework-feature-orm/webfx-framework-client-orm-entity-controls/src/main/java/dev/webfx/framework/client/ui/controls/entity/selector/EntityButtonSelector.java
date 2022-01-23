755c48e24a8b3615b389f7fab6ab81cf33788a4f
Bruno Salmon <dev.salmonb@gmail.com>
2022-01-23 14:19:44 +0000

    Added computation of named parameter values in EntityButtonSelector

--- webfx-framework-feature-orm/webfx-framework-client-orm-entity-controls/src/main/java/dev/webfx/framework/client/ui/controls/entity/selector/EntityButtonSelector.java
+++ webfx-framework-feature-orm/webfx-framework-client-orm-entity-controls/src/main/java/dev/webfx/framework/client/ui/controls/entity/selector/EntityButtonSelector.java
@@ -1,5 +1,31 @@
 package dev.webfx.framework.client.ui.controls.entity.selector;
 
+import dev.webfx.extras.cell.renderer.ValueRenderer;
+import dev.webfx.extras.cell.renderer.ValueRendererFactory;
+import dev.webfx.extras.cell.renderer.ValueRenderingContext;
+import dev.webfx.extras.visual.VisualResult;
+import dev.webfx.extras.visual.controls.grid.SkinnedVisualGrid;
+import dev.webfx.extras.visual.controls.grid.VisualGrid;
+import dev.webfx.framework.client.orm.reactive.mapping.dql_to_entities.ReactiveEntitiesMapper;
+import dev.webfx.framework.client.orm.reactive.mapping.entities_to_grid.EntityColumn;
+import dev.webfx.framework.client.orm.reactive.mapping.entities_to_visual.ReactiveVisualMapper;
+import dev.webfx.framework.client.orm.reactive.mapping.entities_to_visual.VisualEntityColumnFactory;
+import dev.webfx.framework.client.ui.controls.button.ButtonFactoryMixin;
+import dev.webfx.framework.shared.orm.domainmodel.DataSourceModel;
+import dev.webfx.framework.shared.orm.domainmodel.DomainClass;
+import dev.webfx.framework.shared.orm.domainmodel.DomainModel;
+import dev.webfx.framework.shared.orm.dql.DqlStatement;
+import dev.webfx.framework.shared.orm.dql.DqlStatementBuilder;
+import dev.webfx.framework.shared.orm.entity.Entity;
+import dev.webfx.framework.shared.orm.entity.EntityList;
+import dev.webfx.framework.shared.orm.entity.EntityStore;
+import dev.webfx.framework.shared.orm.expression.CollectOptions;
+import dev.webfx.framework.shared.orm.expression.Expression;
+import dev.webfx.framework.shared.orm.expression.terms.ExpressionArray;
+import dev.webfx.framework.shared.orm.expression.terms.Parameter;
+import dev.webfx.platform.shared.util.Arrays;
+import dev.webfx.platform.shared.util.async.Handler;
+import dev.webfx.platform.shared.util.function.Callable;
 import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
@@ -9,30 +35,6 @@ import javafx.scene.Node;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
-import dev.webfx.extras.cell.renderer.ValueRenderer;
-import dev.webfx.extras.cell.renderer.ValueRendererFactory;
-import dev.webfx.extras.cell.renderer.ValueRenderingContext;
-import dev.webfx.extras.visual.VisualResult;
-import dev.webfx.extras.visual.controls.grid.SkinnedVisualGrid;
-import dev.webfx.extras.visual.controls.grid.VisualGrid;
-import dev.webfx.framework.client.orm.reactive.mapping.dql_to_entities.ReactiveEntitiesMapper;
-import dev.webfx.framework.client.ui.controls.button.ButtonFactoryMixin;
-import dev.webfx.framework.shared.orm.dql.DqlStatement;
-import dev.webfx.framework.shared.orm.dql.DqlStatementBuilder;
-import dev.webfx.framework.client.orm.reactive.mapping.entities_to_grid.EntityColumn;
-import dev.webfx.framework.client.orm.reactive.mapping.entities_to_visual.ReactiveVisualMapper;
-import dev.webfx.framework.client.orm.reactive.mapping.entities_to_visual.VisualEntityColumnFactory;
-import dev.webfx.framework.shared.orm.domainmodel.DataSourceModel;
-import dev.webfx.framework.shared.orm.domainmodel.DomainClass;
-import dev.webfx.framework.shared.orm.domainmodel.DomainModel;
-import dev.webfx.framework.shared.orm.entity.Entity;
-import dev.webfx.framework.shared.orm.entity.EntityList;
-import dev.webfx.framework.shared.orm.entity.EntityStore;
-import dev.webfx.framework.shared.orm.expression.Expression;
-import dev.webfx.framework.shared.orm.expression.terms.ExpressionArray;
-import dev.webfx.platform.shared.util.Arrays;
-import dev.webfx.platform.shared.util.async.Handler;
-import dev.webfx.platform.shared.util.function.Callable;
 
 import java.util.List;
 import java.util.function.Predicate;
@@ -48,6 +50,7 @@ public class EntityButtonSelector<E extends Entity> extends ButtonSelector<E> {
     private final ObjectProperty<VisualResult> deferredVisualResult = new SimpleObjectProperty<>();
     private Object jsonOrClass;
     private final DataSourceModel dataSourceModel;
+    private DomainClass entityClass; // the domain class found from jsonOrClass & dataSourceModel
     private Expression<E> renderingExpression;
     private ValueRenderer entityRenderer;
 
@@ -56,6 +59,8 @@ public class EntityButtonSelector<E extends Entity> extends ButtonSelector<E> {
     private List<E> restrictedFilterList;
     private VisualGrid dialogVisualGrid;
     private String searchCondition;
+    // Named parameters within the search condition (extracted after expression parsing)
+    private Parameter[] searchConditionNamedParameters; // Ex:
 
     // Good to put a limit especially for low-end mobiles
     private int adaptiveLimit = 6; // starting with 6 entries (fit with drop down/up) but can be increased in modal in dependence of the available height
@@ -90,7 +95,7 @@ public class EntityButtonSelector<E extends Entity> extends ButtonSelector<E> {
             DqlStatement dqlStatement = new DqlStatementBuilder(jsonOrClass).build();
             DomainModel domainModel = dataSourceModel.getDomainModel();
             Object domainClassId = dqlStatement.getDomainClassId();
-            DomainClass entityClass = domainModel.getClass(domainClassId);
+            entityClass = domainModel.getClass(domainClassId);
             if (dqlStatement.getColumns() != null) {
                 EntityColumn<E>[] entityColumns = VisualEntityColumnFactory.get().fromJsonArrayOrExpressionsDefinition(dqlStatement.getColumns(), entityClass);
                 renderingExpression = new ExpressionArray<>(Arrays.map(entityColumns, expressionColumn -> expressionColumn.parseExpressionDefinitionIfNecessary(entityClass).getDisplayExpression(), Expression[]::new));
@@ -99,7 +104,7 @@ public class EntityButtonSelector<E extends Entity> extends ButtonSelector<E> {
             String fields = dqlStatement.getFields();
             if (renderingExpression == null && fields != null)
                 renderingExpression = entityClass.parseExpression(fields);
-            searchCondition = entityClass.getSearchCondition();
+            setSearchCondition(entityClass.getSearchCondition());
         }
         entityRenderer = renderingExpression == null ? null : ValueRendererFactory.getDefault().createValueRenderer(renderingExpression.getType());
         forceDialogRebuiltOnNextShow();
@@ -107,6 +112,18 @@ public class EntityButtonSelector<E extends Entity> extends ButtonSelector<E> {
 
     public void setSearchCondition(String searchCondition) {
         this.searchCondition = searchCondition;
+        searchConditionNamedParameters = null;
+    }
+
+    private Parameter[] getSearchConditionNamedParameters() {
+        if (searchConditionNamedParameters == null) {
+            // We parse the search condition and collect the terms including the parameters
+            CollectOptions collectOptions = new CollectOptions().setIncludeParameter(true).setTraverseSqlExpressible(true);
+            entityClass.parseExpression(searchCondition).collect(collectOptions);
+            // Then we filter the parameters from the collected terms and put them in an array
+            searchConditionNamedParameters = collectOptions.getCollectedTerms().stream().filter(e -> e instanceof Parameter).map(e -> (Parameter) e).toArray(Parameter[]::new);
+        }
+        return searchConditionNamedParameters;
     }
 
     public void setLoadingStore(EntityStore loadingStore) {
@@ -143,8 +160,9 @@ public class EntityButtonSelector<E extends Entity> extends ButtonSelector<E> {
             if (isSearchEnabled())
                 entityDialogMapper
                         .ifTrimNotEmpty(searchTextProperty(), s -> {
-                            setSearchParameters(s, entityDialogMapper.getReactiveEntitiesMapper().getStore());
-                            return where(searchCondition);
+                            EntityStore store = entityDialogMapper.getReactiveEntitiesMapper().getStore();
+                            setSearchParameters(s, store);
+                            return where(searchCondition, java.util.Arrays.stream(getSearchConditionNamedParameters()).map(e -> e.evaluate(null, store.getEntityDataWriter())).toArray());
                         })
                         .always(dialogHeightProperty(), height -> limit("?", updateAdaptiveLimit(height)));
             //dialogDataGrid.setOnMouseClicked(e -> {if (e.isPrimaryButtonDown() && e.getClickCount() == 1) onDialogOk(); });
