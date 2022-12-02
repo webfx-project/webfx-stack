0570241ad8ae06e9dce26e437f42e6aefd899b84
Bruno Salmon <dev.salmonb@gmail.com>
2022-12-02 18:26:43 +0000

    Made Vert.x event bus configurable

--- webfx-stack-com-bus-json-vertx/src/main/java/module-info.java
+++ webfx-stack-com-bus-json-vertx/src/main/java/module-info.java
@@ -8,17 +8,24 @@ module webfx.stack.com.bus.json.vertx {
     requires io.vertx.web;
     requires webfx.platform.async;
     requires webfx.platform.json;
+    requires webfx.platform.util;
     requires webfx.platform.vertx.common;
     requires webfx.stack.com.bus;
     requires webfx.stack.com.bus.json;
     requires webfx.stack.com.bus.json.server;
+    requires webfx.stack.conf;
+    requires webfx.stack.conf.resource;
     requires webfx.stack.session.state;
     requires webfx.stack.session.vertx;
 
     // Exported packages
     exports dev.webfx.stack.com.bus.spi.impl.json.vertx;
 
+    // Resources packages
+    opens dev.webfx.stack.com.bus.spi.impl.json.vertx;
+
     // Provided services
     provides dev.webfx.stack.com.bus.spi.BusServiceProvider with dev.webfx.stack.com.bus.spi.impl.json.vertx.VertxBusServiceProvider;
+    provides dev.webfx.stack.conf.spi.ConfigurationConsumer with dev.webfx.stack.com.bus.spi.impl.json.vertx.VertxBusConfigurationConsumer;
 
 }
