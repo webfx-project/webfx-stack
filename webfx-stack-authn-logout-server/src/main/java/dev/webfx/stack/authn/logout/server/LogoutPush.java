4a83d202942115efab64ba0c308f1192a25d2f3c
Bruno Salmon <dev.salmonb@gmail.com>
2022-12-02 18:38:43 +0000

    Added LogoutPush utility class in a separate module

---
+++ webfx-stack-authn-logout-server/src/main/java/dev/webfx/stack/authn/logout/server/LogoutPush.java
@@ -0,0 +1,18 @@
+package dev.webfx.stack.authn.logout.server;
+
+import dev.webfx.platform.async.Future;
+import dev.webfx.stack.session.state.ThreadLocalStateHolder;
+import dev.webfx.stack.push.server.PushServerService;
+import dev.webfx.stack.session.state.StateAccessor;
+import dev.webfx.stack.session.state.LogoutUserId;
+
+/**
+ * @author Bruno Salmon
+ */
+public class LogoutPush {
+
+    public static Future<Void> pushLogoutMessageToClient() {
+        String runId = ThreadLocalStateHolder.getRunId();
+        return PushServerService.pushState(StateAccessor.setUserId(null, LogoutUserId.LOGOUT_USER_ID), runId);
+    }
+}
---
+++ webfx-stack-authn-logout-server/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.stack.authn.logout.server {
+
+    // Direct dependencies modules
+    requires webfx.platform.async;
+    requires webfx.stack.push.server;
+    requires webfx.stack.session.state;
+
+    // Exported packages
+    exports dev.webfx.stack.authn.logout.server;
+
+}
