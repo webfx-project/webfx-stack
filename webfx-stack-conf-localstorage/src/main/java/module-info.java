108a65505dd1d964ecc1fc873390ccfa6999c49c
Bruno Salmon <dev.salmonb@gmail.com>
2022-12-12 11:48:19 +0000

    Added webfx-stack-conf-localstorage

---
+++ webfx-stack-conf-localstorage/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.stack.conf.localstorage {
+
+    // Direct dependencies modules
+    requires java.base;
+    requires webfx.platform.async;
+    requires webfx.platform.console;
+    requires webfx.platform.storage;
+    requires webfx.platform.util;
+    requires webfx.stack.conf;
+
+    // Exported packages
+    exports dev.webfx.stack.conf.spi.impl.localstorage;
+
+    // Provided services
+    provides dev.webfx.stack.conf.spi.ConfigurationSupplier with dev.webfx.stack.conf.spi.impl.localstorage.LocalStorageConfigurationSupplier;
+
+}
