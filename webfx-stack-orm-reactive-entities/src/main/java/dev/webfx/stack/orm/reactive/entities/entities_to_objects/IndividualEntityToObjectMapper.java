822b5d8828ef4fab77ce7b367eb384a97a0f518d
Bruno Salmon <dev.salmonb@gmail.com>
2023-07-21 14:22:32 +0100

    Added IndividualEntityToObjectMapper.createFactory() utility method

--- webfx-stack-orm-reactive-entities/src/main/java/dev/webfx/stack/orm/reactive/entities/entities_to_objects/IndividualEntityToObjectMapper.java
+++ webfx-stack-orm-reactive-entities/src/main/java/dev/webfx/stack/orm/reactive/entities/entities_to_objects/IndividualEntityToObjectMapper.java
@@ -2,15 +2,47 @@ package dev.webfx.stack.orm.reactive.entities.entities_to_objects;
 
 import dev.webfx.stack.orm.entity.Entity;
 
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 /**
  * @author Bruno Salmon
  */
-public interface IndividualEntityToObjectMapper<E extends Entity, T> {
+public interface IndividualEntityToObjectMapper<E extends Entity, O> {
 
-    T getMappedObject();
+    O getMappedObject();
 
     void onEntityChangedOrReplaced(E entity);
 
     void onEntityRemoved(E entity);
+
+    static <E extends Entity, O, V> Function<E, IndividualEntityToObjectMapper<E, O>> createFactory(Supplier<V> viewFactory, BiConsumer<V, E> viewEntitySetter, Function<V, O> viewObjectGetter) {
+        return e -> {
+            IndividualEntityToObjectMapper<E, O> mapper = create(viewFactory, viewEntitySetter, viewObjectGetter);
+            mapper.onEntityChangedOrReplaced(e);
+            return mapper;
+        };
+    }
+
+   static <E extends Entity, O, V> IndividualEntityToObjectMapper<E, O> create(Supplier<V> viewFactory, BiConsumer<V, E> viewEntitySetter, Function<V, O> viewObjectGetter) {
+        return new IndividualEntityToObjectMapper<>() {
+
+            private final V view = viewFactory.get();
+
+            @Override
+            public O getMappedObject() {
+                return viewObjectGetter.apply(view);
+            }
+
+            @Override
+            public void onEntityChangedOrReplaced(E entity) {
+                viewEntitySetter.accept(view, entity);
+            }
+
+            @Override
+            public void onEntityRemoved(E entity) { }
+        };
+    }
 
 }
