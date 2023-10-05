d71f2641ab8bcf62a8f1a18a4c94e3a020d085cf
Bruno Salmon <dev.salmonb@gmail.com>
2023-10-05 12:11:49 +0100

    Added FXUserClaims

--- webfx-stack-session-state-client-fx/src/main/java/module-info.java
+++ webfx-stack-session-state-client-fx/src/main/java/module-info.java
@@ -9,6 +9,7 @@ module webfx.stack.session.state.client.fx {
     requires webfx.platform.console;
     requires webfx.platform.uischeduler;
     requires webfx.platform.util;
+    requires webfx.stack.authn;
     requires webfx.stack.session;
     requires webfx.stack.session.state;
     requires webfx.stack.session.state.client;
