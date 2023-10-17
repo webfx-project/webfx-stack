a8c0c83effcdb6e29326b7977c46a89bed997517
Bruno Salmon <dev.salmonb@gmail.com>
2023-10-17 15:58:58 +0100

    Added other parameters to CORS policy

--- webfx-stack-http-server-vertx-plugin/src/main/java/dev/webfx/stack/http/server/vertx/VertxHttpRouterConfigurator.java
+++ webfx-stack-http-server-vertx-plugin/src/main/java/dev/webfx/stack/http/server/vertx/VertxHttpRouterConfigurator.java
@@ -3,6 +3,7 @@ package dev.webfx.stack.http.server.vertx;
 import dev.webfx.platform.ast.ReadOnlyAstArray;
 import dev.webfx.platform.vertx.common.VertxInstance;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.*;
@@ -19,10 +20,23 @@ final class VertxHttpRouterConfigurator {
         Router router = Router.router(vertx);
 
         // Logging web requests
-        router.route("/*").handler(LoggerHandler.create());
+        router.route().handler(LoggerHandler.create());
 
         // The session store to use
-        router.route("/*").handler(SessionHandler.create(VertxInstance.getSessionStore()));
+        router.route().handler(SessionHandler.create(VertxInstance.getSessionStore()));
+
+        // Wildcard CORS policy. This is temporary, before CORS configuration is implemented.
+        router.route().handler(CorsHandler.create()
+                .addOrigin("*")
+                .allowedMethod(HttpMethod.GET)
+                .allowedMethod(HttpMethod.POST)
+                .allowedMethod(HttpMethod.OPTIONS)
+                .allowCredentials(true)
+                .allowedHeader("Access-Control-Allow-Method")
+                .allowedHeader("Access-Control-Allow-Origin")
+                .allowedHeader("Access-Control-Allow-Credentials")
+                .allowedHeader("Content-Type")
+        );
 
         // GWT perfect caching (xxx.cache.xxx files will never change again)
         router.routeWithRegex(".*\\.cache\\..*").handler(routingContext -> {
@@ -37,9 +51,6 @@ final class VertxHttpRouterConfigurator {
             routingContext.response().putHeader("cache-control", "no-cache");
             routingContext.next();
         });
-
-        // Wildcard CORS policy. This is temporary, before CORS configuration is implemented.
-        router.route("/*").handler(CorsHandler.create().addOrigin("*"));
 
         return router;
     }
