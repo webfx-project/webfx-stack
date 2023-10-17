cbc99677f61abc3d0fd324c1f38b1614ec8d9751
Bruno Salmon <dev.salmonb@gmail.com>
2023-10-17 21:07:57 +0100

    Removed CORS policy, and included "*.nocache.*" files in no-cache control

--- webfx-stack-http-server-vertx-plugin/src/main/java/dev/webfx/stack/http/server/vertx/VertxHttpRouterConfigurator.java
+++ webfx-stack-http-server-vertx-plugin/src/main/java/dev/webfx/stack/http/server/vertx/VertxHttpRouterConfigurator.java
@@ -3,7 +3,6 @@ package dev.webfx.stack.http.server.vertx;
 import dev.webfx.platform.ast.ReadOnlyAstArray;
 import dev.webfx.platform.vertx.common.VertxInstance;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.*;
@@ -25,19 +24,6 @@ final class VertxHttpRouterConfigurator {
         // The session store to use
         router.route().handler(SessionHandler.create(VertxInstance.getSessionStore()));
 
-        // Wildcard CORS policy. This is temporary, before CORS configuration is implemented.
-        router.route().handler(CorsHandler.create()
-                .addOrigin("*")
-                .allowedMethod(HttpMethod.GET)
-                .allowedMethod(HttpMethod.POST)
-                .allowedMethod(HttpMethod.OPTIONS)
-                .allowCredentials(true)
-                .allowedHeader("Access-Control-Allow-Method")
-                .allowedHeader("Access-Control-Allow-Origin")
-                .allowedHeader("Access-Control-Allow-Credentials")
-                .allowedHeader("Content-Type")
-        );
-
         // GWT perfect caching (xxx.cache.xxx files will never change again)
         router.routeWithRegex(".*\\.cache\\..*").handler(routingContext -> {
             routingContext.response().putHeader("cache-control", "public, max-age=31556926");
@@ -46,8 +32,8 @@ final class VertxHttpRouterConfigurator {
 
         // SPA root page shouldn't be cached (to always return the latest version with the latest GWT compilation)
         // We assume the SPA is hosted under the root / or under any path ending with / or /index.html or any path
-        // including /#/ (which is used for UI routing).
-        router.routeWithRegex(".*/|.*/index.html|.*/#/.*").handler(routingContext -> {
+        // including /#/ (which is used for UI routing). This applies also to xxx.nocache.xxx GWT files.
+        router.routeWithRegex(".*/|.*/index.html|.*/#/.*|.*\\.nocache\\..*").handler(routingContext -> {
             routingContext.response().putHeader("cache-control", "no-cache");
             routingContext.next();
         });
