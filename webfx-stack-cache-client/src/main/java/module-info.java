4977da2350e21c01c8b6f616cb18bc3e11508dcf
Bruno Salmon <dev.salmonb@gmail.com>
2024-01-05 16:43:33 +0000

    Added webfx-stack-cache & webfx-stack-cache-client modules (2)

---
+++ webfx-stack-cache-client/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.stack.cache.client {
+
+    // Direct dependencies modules
+    requires webfx.stack.cache;
+    requires webfx.stack.session;
+    requires webfx.stack.session.state.client.fx;
+
+    // Exported packages
+    exports dev.webfx.stack.cache.client;
+
+}
---
+++ webfx-stack-cache/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.stack.cache {
+
+    // Exported packages
+    exports dev.webfx.stack.cache;
+
+}
