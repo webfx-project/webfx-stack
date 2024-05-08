0b452edbd59d63f6a64c2dfaf66d04f71fc89d05
Bruno Salmon <dev.salmonb@gmail.com>
2024-05-08 11:14:24 +0100

    Prevented showing dialog multiple times in MasterSlaveLinker

--- webfx-stack-orm-entity-controls/src/main/java/dev/webfx/stack/orm/entity/controls/entity/masterslave/MasterSlaveLinker.java
+++ webfx-stack-orm-entity-controls/src/main/java/dev/webfx/stack/orm/entity/controls/entity/masterslave/MasterSlaveLinker.java
@@ -12,6 +12,7 @@ import java.util.Objects;
 public class MasterSlaveLinker<T> {
 
     private boolean internalMasterChange;
+    private boolean isSlaveSwitchApprovalDialogShowing;
 
     private final ObjectProperty<T> masterProperty = new SimpleObjectProperty<>() {
         @Override
@@ -23,11 +24,13 @@ public class MasterSlaveLinker<T> {
             // If the master and slave are already the same, we don't do anything
             if (Objects.equals(getMaster(), getSlave()))
                 return;
-            // Otherwise we check the approval for the slave switch
-            checkSlaveSwitchApproval(false, () -> {
-                // If it has been approved, we apply the switch (slave = master) and ask the slave editor to edit it
-                slaveEditor.setSlave(getMaster());
-            });
+            // Otherwise we check the approval for the slave switch (unless a dialog is already showing)
+            if (!isSlaveSwitchApprovalDialogShowing) {
+                checkSlaveSwitchApproval(false, () -> {
+                    // If it has been approved, we apply the switch (slave = master) and ask the slave editor to edit it
+                    slaveEditor.setSlave(getMaster());
+                });
+            }
         }
     };
 
@@ -58,12 +61,14 @@ public class MasterSlaveLinker<T> {
         if (getSlave() == null || !slaveEditor.hasChanges())
             callOnApprovalCallback(clearMasterOnApproval, onApprovalCallback);
         else { // Otherwise we need to ask the user for approval in a dialog
+            isSlaveSwitchApprovalDialogShowing = true;
             Platform.runLater(() -> { // The reason why we postpone the call to show the dialog is to ensure that the
                 // dialog area (in Modality) matches the current activity. Otherwise, if this code is ran while resuming
                 // the activity (which happens when masterEntityProperty is bound to the selectedEntityProperty of a
                 // ReactiveVisualMapper which becomes active again on activity resume), the dialog area hasn't been
                 // updated yet, and the dialog would be displayed in the leaving activity instead of the entering activity.
                 slaveEditor.showSlaveSwitchApprovalDialog(approved -> {
+                    isSlaveSwitchApprovalDialogShowing = false;
                     // If the user approved the switch (which will discard the changes on the previous slave)
                     if (approved) // then we call the approval callback
                         callOnApprovalCallback(clearMasterOnApproval, onApprovalCallback);
