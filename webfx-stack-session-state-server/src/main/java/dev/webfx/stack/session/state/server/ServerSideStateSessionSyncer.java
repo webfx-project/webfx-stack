421ab2e73d2b74d67ad733725ac4da3f3772ce3f
Bruno Salmon <dev.salmonb@gmail.com>
2024-08-18 22:39:32 +0100

    Fixed issues with webfx session management in Vert.x (1 session per socket)

--- webfx-stack-session-state-server/src/main/java/dev/webfx/stack/session/state/server/ServerSideStateSessionSyncer.java
+++ webfx-stack-session-state-server/src/main/java/dev/webfx/stack/session/state/server/ServerSideStateSessionSyncer.java
@@ -37,18 +37,14 @@ public final class ServerSideStateSessionSyncer {
     public static Future<Session> syncServerSessionFromIncomingClientState(Session serverSession, Object clientState) {
         // serverSession.id <= clientState.serverSessionId ? ONLY ON NEW SERVER SESSION
         String requestedServerSessionId = StateAccessor.getServerSessionId(clientState);
-        boolean newServerSession = SessionAccessor.getRunId(serverSession) == null;
-        if (requestedServerSessionId != null && newServerSession && !Objects.equals(requestedServerSessionId, serverSession.id())) {
-            Promise<Session> promise = Promise.promise();
-            SessionService.getSessionStore().get(requestedServerSessionId).onComplete(ar -> {
-                Session requestedSession = ar.result();
-                syncFixedServerSessionFromIncomingClientStateWithUserIdCheckFirst(requestedSession != null ? requestedSession : serverSession, clientState, false)
-                        .onFailure(promise::fail)
-                        .onSuccess(promise::complete);
-            });
-            return promise.future();
+        String serverSessionRunId = SessionAccessor.getRunId(serverSession);
+        boolean isNewServerSession = serverSessionRunId == null;
+        if (requestedServerSessionId != null /*&& isNewServerSession*/ && !Objects.equals(requestedServerSessionId, serverSession.id())) {
+            return SessionService.getSessionStore().get(requestedServerSessionId)
+                .compose(loadedSession ->
+                    syncFixedServerSessionFromIncomingClientStateWithUserIdCheckFirst(loadedSession != null ? loadedSession : serverSession, clientState, false));
         }
-        return syncFixedServerSessionFromIncomingClientStateWithUserIdCheckFirst(serverSession, clientState, newServerSession);
+        return syncFixedServerSessionFromIncomingClientStateWithUserIdCheckFirst(serverSession, clientState, isNewServerSession);
     }
 
     private static Future<Session> syncFixedServerSessionFromIncomingClientStateWithUserIdCheckFirst(Session serverSession, Object clientState, boolean forceStore) {
@@ -57,34 +53,35 @@ public final class ServerSideStateSessionSyncer {
             return syncFixedServerSessionFromIncomingClientState(serverSession, clientState, forceStore);
         Promise<Session> promise = Promise.promise();
         ThreadLocalStateHolder.runWithState(clientState, () -> userIdChecker.apply(userId))
-                .onComplete(ar -> {
-                    // Setting the new user id (should be the same as the passed on if valid, or something like "INVALID" if not)
-                    Object finalUserId = ar.result();
-                    Console.log("️🛡🛡🛡🛡🛡 UserIdCheck: userId=" + userId + " => finalUserId = " + finalUserId);
-                    // If the user identity check failed, we log out the user
-                    if (finalUserId == null)
-                        finalUserId = LogoutUserId.LOGOUT_USER_ID;
-                    // Memorizing the final user id in the client state
-                    StateAccessor.setUserId(clientState, finalUserId);
-                    // We continue with the normal session <-> state sync process
-                    syncFixedServerSessionFromIncomingClientState(serverSession, clientState, forceStore)
-                            .onFailure(promise::fail)
-                            .onSuccess(promise::complete);
-                    // We set the runId from the session if not set in the state
-                    if (StateAccessor.getRunId(clientState) == null)
-                        StateAccessor.setRunId(clientState, SessionAccessor.getRunId(serverSession));
-                    // We log out the client or push the authorizations of the user
-                    ThreadLocalStateHolder.runWithState(clientState, () -> {
-                        // Special case: invalid user => we force a logout
-                        if (LogoutUserId.isLogoutUserId(ThreadLocalStateHolder.getUserId()))
-                            LogoutPush.pushLogoutMessageToClient(); // This will push a logout userId, and subsequently push the new authorizations (see OUTGOING STATE)
+            .onComplete(ar -> {
+                // Setting the new user id (should be the same as the passed on if valid, or something like "INVALID" if not)
+                Object finalUserId = ar.result();
+                Console.log("️🛡🛡🛡🛡🛡 UserIdCheck: userId=" + userId + " => finalUserId = " + finalUserId);
+                // If the user identity check failed, we log out the user
+                if (finalUserId == null)
+                    finalUserId = LogoutUserId.LOGOUT_USER_ID;
+                // Memorizing the final user id in the client state
+                StateAccessor.setUserId(clientState, finalUserId);
+                // We continue with the normal session <-> state sync process
+                syncFixedServerSessionFromIncomingClientState(serverSession, clientState, forceStore)
+                    .onFailure(promise::fail)
+                    .onSuccess(promise::complete);
+                // We set the runId from the session if not set in the state
+                if (StateAccessor.getRunId(clientState) == null)
+                    StateAccessor.setRunId(clientState, SessionAccessor.getRunId(serverSession));
+                // We log out the client or push the authorizations of the user
+                ThreadLocalStateHolder.runWithState(clientState, () -> {
+                    // Special case: invalid user => we force a logout
+                    if (LogoutUserId.isLogoutUserId(ThreadLocalStateHolder.getUserId())) {
+                        LogoutPush.pushLogoutMessageToClient(); // This will push a logout userId, and subsequently push the new authorizations (see OUTGOING STATE)
                         // General case: valid user (probably a user switch from the client, or a reconnection)
-                        else if (userIdAuthorizer != null)
-                            // We ask the authorizer to push the new authorizations for that user
-                            // Note: that push shouldn't contain the userId, otherwise this will create a loop (see OUTGOING STATE).
-                            userIdAuthorizer.apply(null);
-                    });
+                    } else if (userIdAuthorizer != null) {
+                        // We ask the authorizer to push the new authorizations for that user
+                        // Note: that push shouldn't contain the userId, otherwise this will create a loop (see OUTGOING STATE).
+                        userIdAuthorizer.apply(null);
+                    }
                 });
+            });
         return promise.future();
     }
 
@@ -102,9 +99,9 @@ public final class ServerSideStateSessionSyncer {
     }
 
     private static Future<Session> storeServerSession(Session serverSession) {
-        Future<Boolean> future = SessionService.getSessionStore().put(serverSession);
-        future.onFailure(Console::log);
-        return future.map(x -> serverSession);
+        return SessionService.getSessionStore().put(serverSession)
+            .onFailure(Console::log)
+            .map(x -> serverSession);
     }
 
     public static Object syncIncomingClientStateFromServerSession(Object clientState, Session serverSession) {
@@ -122,12 +119,24 @@ public final class ServerSideStateSessionSyncer {
     // Sync methods to be used on server side, when the server is about to send a state generated by the server back to the client
 
     public static Object syncOutgoingServerStateFromServerSessionAndViceVersa(Object serverState, Session serverSession) {
+        Object userId = StateAccessor.getUserId(serverState);
         // If a user id is set in that direction, this means the server switched, logged-in or logged-out the user,
         // so we need in all cases to call the authorizer to push the new authorizations to the client.
         // Note: that authorizations push shouldn't contain the user id to avoid a loop here.
-        Object userId = StateAccessor.getUserId(serverState);
-        if (userId != null && userIdAuthorizer != null)
-            ThreadLocalStateHolder.runWithState(StateAccessor.setUserId(StateAccessor.setRunId(null, SessionAccessor.getRunId(serverSession)), userId), () -> userIdAuthorizer.apply(null));
+        if (userId != null && userIdAuthorizer != null) {
+            // It's important to set the userId and runId in ThreadLocalStateHolder before calling userIdAuthorizer
+            // because it will load the authorizations from userId and push them to the runId client.
+
+            // Most of the time the runId is in the server session, which matches the associated client. An exception to
+            // that rule is the magic link, where the magic link client and the login client are different. If the magic
+            // link is valid, the authorizations must be pushed to the login client and not to the magic link client
+            // associated with this session. The magic link AuthenticationGatewayProvider indicated this by setting the
+            // login client runId in the server state.
+
+            String runId = SessionAccessor.getRunId(serverSession);
+            Object state = StateAccessor.createUserIdRunIdState(userId, runId);
+            ThreadLocalStateHolder.runWithState(state, () -> userIdAuthorizer.apply(null));
+        }
         // serverSession.id <= serverState.serverSessionId ? NEVER (serverSession.id can't be changed at this point)
         // serverSession.userId <= serverState.userId ? YES IF SET, as this means the server switched or logged-in user, so we memorise that info in the session
         boolean userIdChanged = SessionAccessor.changeUserId(serverSession, userId, true);
