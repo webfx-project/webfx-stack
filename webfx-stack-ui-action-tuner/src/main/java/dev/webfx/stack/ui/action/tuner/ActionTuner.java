f8001e8e9d0d3d1c6013358a4528dc85e8dadc51
Bruno Salmon <dev.salmonb@gmail.com>
2024-11-17 12:56:55 +0000

    Added ActionTuner interface in new webfx-stack-ui-action-tuner module

---
+++ webfx-stack-ui-action-tuner/src/main/java/dev/webfx/stack/ui/action/tuner/ActionTuner.java
@@ -0,0 +1,12 @@
+package dev.webfx.stack.ui.action.tuner;
+
+import dev.webfx.stack.ui.action.Action;
+
+/**
+ * @author Bruno Salmon
+ */
+public interface ActionTuner {
+
+    Action tuneAction(Action action);
+
+}
