c145501b8755e65d5daf968d49b2a5ca08296e5e
Bruno Salmon <dev.salmonb@gmail.com>
2024-11-18 18:15:57 +0000

    Made RouteBackwardRequest & RouteForwardRequest implement HasI18nKey (introduced dependency to I18n - ok?)

---
+++ webfx-stack-routing-uirouter/src/main/java/dev/webfx/stack/routing/uirouter/operations/RouteI18nKeys.java
@@ -0,0 +1,9 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+package dev.webfx.stack.routing.uirouter.operations;
+
+public interface RouteI18nKeys {
+
+    String RouteBackward = "RouteBackward";
+    String RouteForward = "RouteForward";
+
+}
