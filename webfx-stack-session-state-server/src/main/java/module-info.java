43df73238a55ed18dd00006fb696fd5756da8d24
Bruno Salmon <dev.salmonb@gmail.com>
2025-01-28 15:36:28 +0000

    Improved ServerSideStateSessionSyncer encapsulation

--- webfx-stack-session-state-server/src/main/java/module-info.java
+++ webfx-stack-session-state-server/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module webfx.stack.session.state.server {
     // Direct dependencies modules
     requires transitive webfx.platform.async;
     requires webfx.platform.console;
+    requires transitive webfx.platform.util;
     requires webfx.stack.authn.logout.server;
     requires webfx.stack.session;
     requires webfx.stack.session.state;
