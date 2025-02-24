9a92a67c2d5f44e1862b55bb15a2832797391474
Bruno Salmon <dev.salmonb@gmail.com>
2025-02-24 16:28:43 +0000

    Lowercased i18n keys in webfx-stack-i18n-time-plugin

--- webfx-stack-i18n-time-plugin/src/main/java/dev/webfx/stack/i18n/time/I18nTimeFormatProvider.java
+++ webfx-stack-i18n-time-plugin/src/main/java/dev/webfx/stack/i18n/time/I18nTimeFormatProvider.java
@@ -15,36 +15,36 @@ public class I18nTimeFormatProvider implements TimeFormatProvider {
 
     @Override
     public String getMonthName(Month month) {
-        return I18n.getI18nText(month.name().toUpperCase());
+        return I18n.getI18nText(month.name().toLowerCase());
     }
 
     @Override
     public ObservableStringValue monthNameProperty(Month month) {
-        return I18n.i18nTextProperty(month.name().toUpperCase());
+        return I18n.i18nTextProperty(month.name().toLowerCase());
     }
 
     @Override
     public String getDayOfWeekName(DayOfWeek dayOfWeek) {
-        return I18n.getI18nText(dayOfWeek.name().toUpperCase());
+        return I18n.getI18nText(dayOfWeek.name().toLowerCase());
     }
 
     @Override
     public ObservableStringValue dayOfWeekNameProperty(DayOfWeek dayOfWeek) {
-        return I18n.i18nTextProperty(dayOfWeek.name().toUpperCase());
+        return I18n.i18nTextProperty(dayOfWeek.name().toLowerCase());
     }
 
     @Override
     public String getYearMonthName(YearMonth yearMonth) {
         // Commented as [{0}] doesn't work for now because [] is interpreted first (-> produces {0}} and then {0}
         //return I18n.getI18nText("[{0}] {1}", yearMonth.getMonth().name().toUpperCase(), yearMonth.getYear());
-        return I18n.getI18nText("[" + yearMonth.getMonth().name().toUpperCase() + "] {0}", yearMonth.getYear());
+        return I18n.getI18nText("[" + yearMonth.getMonth().name().toLowerCase() + "] {0}", yearMonth.getYear());
     }
 
     @Override
     public ObservableStringValue yearMonthNameProperty(YearMonth yearMonth) {
         // Commented as [{0}] doesn't work for now because [] is interpreted first (-> produces {0}} and then {0}
         //return I18n.i18nTextProperty("[{0}] {1}", yearMonth.getMonth().name().toUpperCase(), yearMonth.getYear());
-        return I18n.i18nTextProperty("[" + yearMonth.getMonth().name().toUpperCase() + "] {0}", yearMonth.getYear());
+        return I18n.i18nTextProperty("[" + yearMonth.getMonth().name().toLowerCase() + "] {0}", yearMonth.getYear());
     }
 
 }
--- webfx-stack-i18n-time-plugin/src/main/java/dev/webfx/stack/i18n/time/TimeI18nKeys.java
+++ webfx-stack-i18n-time-plugin/src/main/java/dev/webfx/stack/i18n/time/TimeI18nKeys.java
@@ -3,25 +3,27 @@ package dev.webfx.stack.i18n.time;
 
 public interface TimeI18nKeys {
 
-    String JANUARY = "JANUARY";
-    String JUNE = "JUNE";
-    String THURSDAY = "THURSDAY";
-    String SUNDAY = "SUNDAY";
-    String MAY = "MAY";
-    String OCTOBER = "OCTOBER";
-    String TUESDAY = "TUESDAY";
-    String FRIDAY = "FRIDAY";
-    String DECEMBER = "DECEMBER";
-    String FEBRUARY = "FEBRUARY";
-    String SEPTEMBER = "SEPTEMBER";
-    String NOVEMBER = "NOVEMBER";
-    String WEDNESDAY = "WEDNESDAY";
-    String MONDAY = "MONDAY";
-    String SATURDAY = "SATURDAY";
-    String MARCH = "MARCH";
-    String AUGUST = "AUGUST";
-    String JULY = "JULY";
-    String WEEK = "WEEK";
-    String APRIL = "APRIL";
+    String november = "november";
+    String saturday = "saturday";
+    String week = "week";
+    String may = "may";
+    String august = "august";
+    String february = "february";
+    String thursday = "thursday";
+    String july = "july";
+    String april = "april";
+    String march = "march";
+    String sunday = "sunday";
+    String tuesday = "tuesday";
+    String june = "june";
+    String september = "september";
+    String january = "january";
+    String wednesday = "wednesday";
+    String days = "days";
+    String friday = "friday";
+    String december = "december";
+    String october = "october";
+    String day = "day";
+    String monday = "monday";
 
 }
