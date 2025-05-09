949ccde56a0265d43377067ad9a788b0be061728
Bruno Salmon <dev.salmonb@gmail.com>
2025-05-09 14:25:32 +0100

    Reduced warnings in I18nProviderImpl comments

--- webfx-stack-i18n/src/main/java/dev/webfx/stack/i18n/spi/impl/I18nProviderImpl.java
+++ webfx-stack-i18n/src/main/java/dev/webfx/stack/i18n/spi/impl/I18nProviderImpl.java
@@ -52,14 +52,14 @@ public class I18nProviderImpl implements I18nProvider {
             public <T> T raiseValue(Object value, Class<T> raisedClass, Object... args) {
                 if (value instanceof TokenSnapshot) {
                     TokenSnapshot tokenSnapshot = (TokenSnapshot) value;
-                    // value = tokenSnapshot.tokenValue; // this value may be deprecated (see explanation below)
+                    // Old code: value = tokenSnapshot.tokenValue; // this value may be deprecated (see explanation below).
                     // Although args are not handled here (they will be later), it's possible that the i18nKey internal
-                    // state has changed. This happens for example in BookEventActivity (Modality front-office) with
+                    // state has changed. This happens, for example, in BookEventActivity (Modality front-office) with
                     // new I18nSubKey("expression: venue.address", FXEvent.eventProperty()), loadedProperty)
-                    // where parentI18nKey = FX.eventProperty() is an entity that may not be completely loaded on first
-                    // i18n evaluation. The argument loadedProperty is actually not used in the evaluation itself, its
-                    // purpose is just to trigger a new i18n evaluation once the entity is completely loaded. At this
-                    // point, we need to refresh the value with a new i18n evaluation.
+                    // where parentI18nKey = FX.eventProperty() is an entity that may not be completely loaded on the
+                    // first i18n evaluation. The argument loadedProperty is actually not used in the evaluation itself;
+                    // its purpose is just to trigger a new i18n evaluation once the entity is completely loaded. At
+                    // this point, we need to refresh the value with a new i18n evaluation.
                     value = tokenSnapshot.i18nProvider.getFreshTokenValueFromSnapshot(tokenSnapshot);
                     if (value == null)
                         return null; // TODO: find a way to tell the ValueConverterRegistry that null is the actual final value
@@ -97,8 +97,8 @@ public class I18nProviderImpl implements I18nProvider {
         if (initialLanguage == null)
             initialLanguage = defaultLanguage;
         setLanguage(initialLanguage);
-        // We use FXRaiser to interpret arguments (see I18nProvider default methods), but we add here a final step in
-        // order to interpret possible brackets AFTER arguments resolution. For example, i18n TimeFormat defines a key
+        // We use FXRaiser to interpret arguments (see I18nProvider default methods), but we add here a final step
+        // to interpret possible brackets AFTER arguments resolution. For example, i18n TimeFormat defines a key
         // called yearMonth2 whose value is [{1}] {0} (in English), which after arguments resolution can be [february] 25
         // and [february] still needs to be interpreted by i8n. That's what we are doing here.
         i18nFxValueRaiser = new FXValueRaiser() {
@@ -106,7 +106,7 @@ public class I18nProviderImpl implements I18nProvider {
             public <T> T raiseValue(Object value, Class<T> raisedClass, Object... args) {
                 // Doing default arguments resolution
                 T raisedValue = FXRaiser.getFxValueRaiserInstance().raiseValue(value, raisedClass, args);
-                // Doing post bracket interpretation (works only for TEXT token)
+                // Doing post-bracket interpretation (works only for TEXT token)
                 if (raisedValue instanceof String && ((String) raisedValue).contains("[")) {
                     Dictionary dictionary = getDictionary();
                     raisedValue = (T) interpretBracketsAndDefaultInTokenValue(raisedValue, null, "", DefaultTokenKey.TEXT, dictionary, false, getDefaultDictionary(), true);
@@ -170,7 +170,7 @@ public class I18nProviderImpl implements I18nProvider {
         if (i18nKey != null) {
             Object messageKey = i18nKeyToDictionaryMessageKey(i18nKey);
             tokenValue = dictionary == null ? null : dictionary.getMessageTokenValue(messageKey, tokenKey, false);
-            // Message key prefix & suffix interpretation
+            // Message key prefix and suffix interpretation
             if (tokenValue == null && !skipMessageKeyInterpretation && messageKey instanceof String) {
                 String sKey = (String) messageKey;
                 int length = Strings.length(sKey);
@@ -211,21 +211,21 @@ public class I18nProviderImpl implements I18nProvider {
                 }
                 // Case transformer keys
                 if (tokenValue == null && length > 1 && dictionary != null) {
-                    // Second search but ignoring case
+                    // Second search but ignoring the case
                     tokenValue = dictionary.getMessageTokenValue(messageKey, tokenKey, true);
                     if (tokenValue != null) { // Yes, we found a value this time!
                         String sValue = Strings.toString(tokenValue);
-                        if (sKey.equals(sKey.toUpperCase())) // key was upper case ? => we upper case the value
+                        if (sKey.equals(sKey.toUpperCase())) // was key in uppercase? => we uppercase the value
                             tokenValue = sValue.toUpperCase();
-                        else if (sKey.equals(sKey.toLowerCase())) // key was lower case ? => we lower case the value
+                        else if (sKey.equals(sKey.toLowerCase())) // was key in lowercase? => we lowercase the value
                             tokenValue = sValue.toLowerCase();
                         else if (!sValue.isEmpty()) {
                             char firstCharKey = sKey.charAt(0);
                             char firstCharValue = sValue.charAt(0);
-                            if (Character.isUpperCase(firstCharKey)) { // first letter uppercase ? => we upper case first letter in value
+                            if (Character.isUpperCase(firstCharKey)) { // was the first letter in uppercase? => we uppercase the first letter in value
                                 if (!Character.isUpperCase(firstCharValue))
                                     tokenValue = Character.toUpperCase(firstCharValue) + sValue.substring(1);
-                            } else { // first letter lowercase ? => we lower case first letter in value
+                            } else { // was the first letter lowercase? => we lowercase the first letter in value
                                 if (!Character.isLowerCase(firstCharValue))
                                     tokenValue = Character.toLowerCase(firstCharValue) + sValue.substring(1);
                             }
@@ -235,7 +235,7 @@ public class I18nProviderImpl implements I18nProvider {
             }
             tokenValue = interpretBracketsAndDefaultInTokenValue(tokenValue, messageKey, i18nKey, tokenKey, dictionary, skipDefaultDictionary, originalDictionary, skipMessageLoading);
         }
-        // Temporary code which is a workaround for the yaml parser not able to parse line feeds in strings.
+        // Temporary code which is a workaround for the YAML parser not able to parse line feeds in strings.
         if (tokenValue instanceof String) // TODO: remove this workaround once yaml parser is fixed
             tokenValue = ((String) tokenValue).replace("\\n", "\n");
         if (tokenValuePrefix != null)
@@ -344,11 +344,11 @@ public class I18nProviderImpl implements I18nProvider {
     public boolean refreshMessageTokenProperties(Object freshI18nKey) {
         // Getting the message map to refresh
         Map<TokenKey, Reference<Property<TokenSnapshot>>> messageMap = liveDictionaryTokenProperties.get(freshI18nKey);
-        // Note that the passed i18nKey may contain fresher internal state than the one in token snapshots. For example
+        // Note that the passed i18nKey may contain a fresher internal state than the one in token snapshots. For example,
         // if a message depends on another object such as the selected item (or Entity in Modality) in a context menu,
-        // the i18nKey can be used to pass that object. This provider doesn't directly manage this case (i.e. use
-        // internal state of i18nKey to interpret the message), but some providers may extend this class to do so by
-        // overriding getDictionaryTokenValueImpl() (ex: ModalityI18nProvider).
+        // the i18nKey can be used to pass that object. This provider doesn't directly manage this case (i.e., use
+        // the internal state of i18nKey to interpret the message), but some providers may extend this class to do so
+        // by overriding getDictionaryTokenValueImpl() (ex: ModalityI18nProvider).
         // So we pass that fresh i18nKey to also ask to refresh the token snapshots with that fresh i18nKey.
         refreshMessageTokenSnapshots(messageMap, freshI18nKey);
         return messageMap != null; // reporting if something has been updated or not
@@ -367,7 +367,7 @@ public class I18nProviderImpl implements I18nProvider {
             Object language = inDefaultLanguage ? getDefaultLanguage() : getLanguage();
             // We schedule the load but defer it because we will probably have many successive calls to this method while
             // the application code is building the user interface. Only after collecting all the keys during these calls
-            // (presumably in the same animation frame) we do the actual load of these keys.
+            // (presumably in the same animation frame) do we do the actual load of these keys.
             dictionaryLoadingScheduled = UiScheduler.scheduleDeferred(() -> {
                 // Making a copy of the keys before clearing it for the next possible schedule
                 Set<Object> loadingKeys = new HashSet<>(keysToLoad); // ConcurrentModificationException observed
@@ -434,8 +434,8 @@ public class I18nProviderImpl implements I18nProvider {
                 Property<TokenSnapshot> tokenProperty = reference == null ? null : reference.get();
                 // Although a tokenProperty is never null at initialization, it can be dropped by the GC since
                 // it is contained in a WeakReference. If this happens, this means that the client software actually
-                // doesn't use it (either never from the beginning or just not anymore after an activity is closed
-                // for example), so we can just remove that entry to release some memory.
+                // doesn't use it (never from the beginning or just not anymore after an activity is closed, for
+                // example), so we can just remove that entry to release some memory.
                 if (tokenProperty == null) // Means the client software doesn't use this token
                     it.remove(); // So we can drop this entry
                 else // Otherwise, the client software still uses it, and we need to update it
@@ -456,7 +456,7 @@ public class I18nProviderImpl implements I18nProvider {
             for (Iterator<Map.Entry<Object, Map<TokenKey, Reference<Property<TokenSnapshot>>>>> it = liveDictionaryTokenProperties.entrySet().iterator(); it.hasNext(); ) {
                 Map.Entry<Object, Map<TokenKey, Reference<Property<TokenSnapshot>>>> messageMapEntry = it.next();
                 refreshMessageTokenSnapshots(messageMapEntry.getValue(), null);
-                // Although a message map is never empty at initialization, it can become empty if all i18nKey,translationPart
+                // Although a message map is never empty at initialization, it can become empty if all i18nKey translationPart
                 // have been removed (as explained above). If this happens, this means that the client software actually
                 // doesn't use this message at all (either never from the beginning or not anymore).
                 if (messageMapEntry.getValue().isEmpty()) // Means the client software doesn't use this i18nKey message
@@ -464,10 +464,4 @@ public class I18nProviderImpl implements I18nProvider {
             }
         }
     }
-
-    /*private String whatToReturnWhenI18nTextIsNotFound(Object i18nKey, I18nPart part) {
-        String value = Strings.toString(i18nKeyToDictionaryMessageKey(i18nKey));
-        return interpretDictionaryValue(i18nKey, part, value);
-    }*/
-
 }
