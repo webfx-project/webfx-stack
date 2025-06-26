6ffe6bf0fe745de4dc253f2ac9d52746578ed470
Bruno Salmon <dev.salmonb@gmail.com>
2025-06-26 12:21:24 +0100

    Fixed orm select: replaced direct SQL evaluation with a persistent fields load (ex: bookingFormUrl)

--- webfx-stack-orm-dql/src/main/java/dev/webfx/stack/orm/dql/sqlcompiler/terms/SymbolSqlCompiler.java
+++ webfx-stack-orm-dql/src/main/java/dev/webfx/stack/orm/dql/sqlcompiler/terms/SymbolSqlCompiler.java
@@ -18,12 +18,20 @@ public final class SymbolSqlCompiler extends AbstractTermSqlCompiler<Symbol> {
 
     @Override
     public void compileExpressionToSql(Symbol e, Options o) {
-        if (e.getExpression() != null)
-            compileChildExpressionToSql(e.getExpression(), o);
-        else {
+        Expression expression = e.getExpression();
+        if (expression != null) { // Ex: a domain field which is actually an expression.
+            // Should the expression be directly compiled in SQL (1), or should we load the persistent fields for its
+            // later evaluation on the client-side (2)?
+            // We want (2) for example, when we load bookingFormUrl in FXFestivals, so that when BookingStarter evaluates
+            // event.onExpressionLoaded("kbs3,bookingFormUrl") it doesn't need to load bookingFormUrl again
+            if (o.generateQueryMapping) // We are in select with query mapping
+                compileExpressionPersistentTermsToSql(expression, o); // (2) => we load persistent fields
+            else // Ex: in a where or order by clause, or inside an As expression to be stored in an alias
+                compileChildExpressionToSql(expression, o); // (1) => we need to evaluate the expression directly in SQL
+        } else {
             Expression foreignField = null;
             Object termDomainClass = e instanceof HasDomainClass /*ex: domain field */ ? ((HasDomainClass) e).getDomainClass() : o.build.getCompilingClass();
-            if (o.readForeignFields && o.clause == SqlClause.SELECT) {
+            if (o.clause == SqlClause.SELECT && o.readForeignFields) {
                 Object foreignClass = o.modelReader.getSymbolForeignDomainClass(termDomainClass, e, false);
                 if (foreignClass != null /* && build.getJoinMapping() == null  to avoid infinite recursion, see item.icon*/)
                     foreignField = o.modelReader.getDomainClassDefaultForeignFields(foreignClass);
