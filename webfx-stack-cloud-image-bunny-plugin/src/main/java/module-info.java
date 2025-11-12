ab5fba2bce1b98973bbe23aa754400191536353b
Bruno Salmon <dev.salmonb@gmail.com>
2025-11-12 11:21:56 +0000

    Added webfx-stack-cloud-image-bunny-plugin (initial version duplicated from cloudinary)

---
+++ webfx-stack-cloud-image-bunny-plugin/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.stack.cloud.image.bunny.plugin {
+
+    // Direct dependencies modules
+    requires webfx.platform.ast;
+    requires webfx.platform.async;
+    requires webfx.platform.blob;
+    requires webfx.platform.conf;
+    requires webfx.platform.console;
+    requires webfx.platform.fetch;
+    requires webfx.platform.util;
+    requires webfx.platform.util.http;
+    requires webfx.stack.cloud.image;
+    requires webfx.stack.hash.sha1;
+
+    // Exported packages
+    exports dev.webfx.stack.cloud.image.impl.bunny;
+
+    // Provided services
+    provides dev.webfx.stack.cloud.image.spi.CloudImageProvider with dev.webfx.stack.cloud.image.impl.bunny.BunnyImageProvider;
+
+}
