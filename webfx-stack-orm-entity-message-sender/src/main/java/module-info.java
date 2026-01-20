8812de74a563cc8dd1dbbb13a7cd870e018cf954
Bruno Salmon <dev.salmonb@gmail.com>
2026-01-20 10:53:42 +0000

    Split entity message sender and receiver into separate modules

---
+++ webfx-stack-orm-entity-message-sender/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.stack.orm.entity.message.sender {
+
+    // Direct dependencies modules
+    requires webfx.stack.com.bus;
+    requires webfx.stack.com.serial;
+    requires webfx.stack.orm.entity;
+
+    // Exported packages
+    exports dev.webfx.stack.orm.entity.message.sender;
+
+}
